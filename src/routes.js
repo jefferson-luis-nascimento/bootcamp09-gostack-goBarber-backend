879a1f8fe07231b44b6f5e69d6f764d042e647b3
jeln666 <je.luis.nascimento@hotmail.com>
2019-10-18 19:01:48 -0300

    Ajustes nas mensagens de erro na formatação do código fonte

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
-import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
