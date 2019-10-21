976cd32b102eb2f59d7f616521f09378a7fe2ac8
jeln666 <je.luis.nascimento@hotmail.com>
2019-10-21 19:38:56 -0300

    Implementado lista de agendamento por prestador de serviço

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,50 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+import File from '../models/File';
+
+class ScheduleController {
+  async index(req, res) {
+    /**
+     * check if user is a provider
+     */
+    const checkUserProvider = User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return res.status(401).json({ error: 'User is not a provider' });
+    }
+
+    const { date } = req.query;
+    const parsedDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        cancelled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      attributes: ['id', 'date', 'user_id'],
+      include: {
+        model: User,
+        as: 'user',
+        attributes: ['id', 'name'],
+        include: {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      },
+      order: ['date'],
+    });
+
+    return res.json(appointments);
+  }
+}
+
+export default new ScheduleController();
