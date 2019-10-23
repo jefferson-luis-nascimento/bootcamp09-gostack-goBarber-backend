4172d8a6302a390565a7009695f34bbf0d77bc03
jeln666 <je.luis.nascimento@hotmail.com>
2019-10-23 15:08:23 -0300

    Implementação de envio de email ao cancelar um agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -7,6 +7,8 @@ import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
 
+import Mail from '../../lib/Mail';
+
 class AppointmentControler {
   async index(req, res) {
     const { page = 1 } = req.query;
@@ -121,7 +123,15 @@ class AppointmentControler {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
     if (!appointment) {
       res.status(404).json({ error: 'Appointment not found' });
@@ -144,6 +154,12 @@ class AppointmentControler {
     appointment.cancelled_at = new Date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento Cancelado',
+      text: 'Você tem um novo cancelamento',
+    });
 
     return res.json(appointment);
   }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '9286691b460cd4',
+    pass: 'dfe9a6dddc84d6',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
